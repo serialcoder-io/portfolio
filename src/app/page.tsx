89e3f673ecea383766518f2800e9951f1fb59ca0
omar <omaranli56@gmail.com>
2025-07-04 14:54:11 +0400

    feat: created new folder components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
+export const metadata = {
+  title: "Home | Omar Anli",
+  description: "Welcome to Omar Anli’s portfolio, a backend and full-stack developer.",
+};
+
+
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
