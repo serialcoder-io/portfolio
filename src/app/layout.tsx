eede57f73f657e8a5de29352c4b7a13a6d9461b6
omar <omaranli56@gmail.com>
2025-07-04 21:52:52 +0400

    feat: installed daisy ui, and added custom dark and light themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import './globals.css';
+
+
 export const metadata = {
   title: {
     default: "Omar Anli | Backend & Full-Stack Developer",
@@ -53,11 +56,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body>{children}</body>
+      <body className="bg-base-200 h-screen">{children}</body>
     </html>
   );
 }
