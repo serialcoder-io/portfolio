ff46e4f7436a2324577e936d4b4c5eea22759669
omar <omaranli56@gmail.com>
2025-07-19 16:39:37 +0400

    feat: display experices with tools

--- src/components/about/work-xp.tsx
+++ src/components/about/work-xp.tsx
@@ -1,39 +1,29 @@
-export default function ExperienceSection() {
-  const experiences = [
-    {
-      id: 1,
-      type: "internship",
-      title: "Backend Developer Intern",
-      description:
-        "Developed RESTful APIs for a leave management system using Django. Implemented dynamic search and optimized database queries for better performance.",
-      startDate: "2023-06-01",
-      endDate: "2023-08-31",
-      company: "Tech Solutions Ltd",
-      tools: ["Django", "PostgreSQL", "HTML", "CSS", "JavaScript"],
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+
+type ExperienceWithTools = Prisma.ExperienceGetPayload<{
+  include: {
+    tools: {
+      include: {
+        tool: true;
+      };
+    };
+  };
+}>;
+
+export default async function ExperienceSection() {
+  const experiences: ExperienceWithTools[] = await prisma.experience.findMany({
+    include: {
+      tools: {
+        include: {
+          tool: true,
+        },
+      },
     },
-    {
-      id: 2,
-      type: "internship",
-      title: "Fullstack Developer Intern",
-      description:
-        "Built a restaurant management platform with Django REST Framework and React. Added reservation and rating features and integrated TailwindCSS for a modern UI.",
-      startDate: "2024-02-01",
-      endDate: "2024-04-30",
-      company: "WebNest Agency",
-      tools: ["Django REST Framework", "React", "TailwindCSS", "SQLite"],
+      orderBy: {
+      endDate: 'desc', // ou 'asc' pour croissant
     },
-    {
-      id: 3,
-      type: "freelance",
-      title: "Mobile App Developer",
-      description:
-        "Developed a mobile application for partner stores to validate vouchers using QR code scanning. Integrated secure authentication and backend validation logic.",
-      startDate: "2025-01-10",
-      endDate: "2025-03-05",
-      company: "Freelance Project",
-      tools: ["React Native", "Java", "REST API"],
-    },
-  ];
+  });
 
   return (
     <section className="space-y-6 py-12">
@@ -46,7 +36,9 @@ export default function ExperienceSection() {
           >
             <h3 className="text-2xl font-semibold text-primary">
               {exp.title}{" "}
-              <span className="text-base text-base-content/70">({exp.type})</span>
+              <span className="text-base text-base-content/70">
+                ({exp.type})
+              </span>
             </h3>
             <p className="text-base-content/80 mb-2">{exp.company}</p>
             <p className="text-sm text-base-content/60 mb-4">
@@ -67,7 +59,7 @@ export default function ExperienceSection() {
                   key={i}
                   className="bg-primary text-primary-content text-xs px-2 py-1 rounded-full"
                 >
-                  {tool}
+                  {tool.tool.name}
                 </span>
               ))}
             </div>
